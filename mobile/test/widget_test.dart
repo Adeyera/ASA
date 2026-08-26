7898ad930fa8db2dda83a30dd7624688da851c3a
Adeyera Toluwani <toluwaniadeyera@gmail.com>
2026-08-26 01:54:53 +0100

    Final code push

--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:african_art_marketplace/main.dart';
-
-void main() {
-  testWidgets('App loads and displays title', (WidgetTester tester) async {
-    await tester.pumpWidget(const AfricanArtApp());
-    expect(find.text('Heritage AR'), findsOneWidget);
-  });
-}
